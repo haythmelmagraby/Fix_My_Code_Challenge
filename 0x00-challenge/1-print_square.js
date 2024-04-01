2afeab8f5ea9ae646472a07cb2aa4a80893464ac
haythmelmagraby <haythmelmagraby@gmail.com>
2024-04-01 14:47:49 +0200

    first commit

---
+++ 0x00-challenge/1-print_square.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+/*
+    Print a square with the character #
+    
+    The size of the square must be the first argument 
+    of the program.
+*/
+
+if (process.argv.length <= 2) {
+    console.error("Missing argument");
+    console.error("Usage: ./1-print_square.js <size>");
+    console.error("Example: ./1-print_square.js 8");
+    process.exit(1);
+}
+
+const size = parseInt(process.argv[2], 10);
+
+if (size && (isNaN(size) || size <= 0)) {
+    console.error("bad size");
+    process.exit(1);
+}
+
+for (let i = 0; i < size; i++) {
+    for (let j = 0; j < size; j++) {
+        process.stdout.write("#");
+    }
+    process.stdout.write("\n");
+}
